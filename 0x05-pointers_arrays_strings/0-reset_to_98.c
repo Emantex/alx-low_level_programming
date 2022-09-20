8544cb65273080de85732f4c71ef3d03d70508a3
Emantex <omanoyeemmanuel@gmail.com>
2022-09-20 14:54:07 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/*
+ * void reset_to_98 - takes im pointer value that is used in derefrencing
+ * @n - takes integer input
+ * returns - none 
+ */   
+
+void reset_to_98(int* n)
+{
+	*n = 98;
+}
