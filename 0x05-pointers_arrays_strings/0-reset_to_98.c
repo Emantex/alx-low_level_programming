3cd1423a21a10dad8fc7cd45d99dcc0cd6c50ff0
Emantex <omanoyeemmanuel@gmail.com>
2022-09-20 15:15:43 -0700

    Pointer in C programming

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-/*
- * void reset_to_98 - takes im pointer value that is used in derefrencing
- * @n - takes integer input
- * returns - none 
- */   
+/**
+ * reset_to_98 - takes the value of a pointer and update it value.
+ * @n: input integer.
+ * Return: no return.
+*/
 
-void reset_to_98(int* n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
