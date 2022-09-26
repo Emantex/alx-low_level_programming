d960e64764f997cf5a16b53ebeae21b5009da646
Emantex <omanoyeemmanuel@gmail.com>
2022-09-26 14:49:27 -0700

    alx tasks

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,26 +1,19 @@
-/*
- * File: 0-memset.c
- * Auth: not
- */
-
 #include "main.h"
-/**
-* _memset - fills the first n bytes of the memory area
-* @s: Apointer to the memory area to be filled.
-* @c: The character to fill the memory area with.
-* @n: The number of bytes to be filled.
-* Description _memset: over there
-*
-* Return: A pointer to be filled memory area @s
-*
-*/
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-	unsigned int index;
-	unsigned char *memory = s, value = c;
 
-	for (index = 0; index < n; index++)
-		memory[index] = value
-	
-	return (memory);
+/**
+ * _memset - fills memory with a constant byte,
+ * @s: memory area.
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
 }
